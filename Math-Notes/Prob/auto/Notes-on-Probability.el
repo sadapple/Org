44eae29b850ac3fa5fe2602a513c75c2cf8e2a0c
sadapple <chuanliustat@gmail.com>
2018-02-22 17:09:32 -0500

    Feb-22-update

---
+++ Math-Notes/Prob/auto/Notes-on-Probability.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "Notes-on-Probability"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "xeCJK")
+   (LaTeX-add-labels
+    "sec:orgc6f9b6b"
+    "sec:org3880f11"
+    "sec:org8f5d350"
+    "sec:org9d44007"
+    "sec:orgca35f3d"
+    "sec:org60a9648"
+    "sec:org737519a"
+    "sec:org329e720"
+    "sec:orgc6c09a5"))
+ :latex)
+
